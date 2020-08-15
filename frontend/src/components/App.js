44b6a04ee278ab1e67e52749fa5b81c3a91bc0eb
George Hong <george.r.hong@duke.edu>
2020-08-15 13:54:57 -0600

    Resolved Issues and Can Use React Youtube now
    used npm install react react-dom.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,58 @@
 import React, { Component } from "react";
+import {useState} from "react";
+import YouTube from "react-youtube";
+
 import { render } from "react-dom";
+const videoIdA = 'XxVg_s8xAms';
+
+// Display Video 
+function Example() {
+  const [videoId, setVideoId] = useState(videoIdA);
+  const [player, setPlayer] = useState(null);
+  const [time, setTime] = useState(0);
+  const [dispComm, dispToggle] = useState(null);
+
+  const onReady = (event) => {
+    // eslint-disable-next-line
+    console.log(`YouTube Player object for videoId: "${videoId}" has been saved to state.`);
+    // Feeds the event.target player into player.
+    setPlayer(event.target);
+  };
+
+  const onPlayVideo = () => {
+    player.playVideo();
+  };
+
+  const onPauseVideo = () => {
+    player.pauseVideo();
+  };
+
+  const toggleComments = () =>{
+    (dispComm === 'hidden') ? dispToggle(null) : dispToggle('hidden');
+  }
+
+  const getTimeStamp = () => {
+    setTime(~~player.getCurrentTime());
+  };
+
+  return (
+    <div>
+      <h1> Sample Lecture Page</h1>
+      <hr></hr>
+      <YouTube videoId={videoId} onReady={onReady} onPause={getTimeStamp}/>
+      <h1> Current Timestamp: {time} </h1>
+      <button type="button" onClick={onPlayVideo} disabled={!player}>
+        Play
+      </button>
+      <button type="button" onClick={onPauseVideo} disabled={!player}>
+        Pause
+      </button>
+      <div visibility={dispComm}>
+        <h3 >(Pause to show comment section)</h3>
+      </div>
+    </div>
+  );
+}
 
 class App extends Component {
   constructor(props) {
@@ -52,4 +105,8 @@ class App extends Component {
 export default App;
 
 const container = document.getElementById("app");
-render(<App />, container);
+render(
+<div>
+    <Example/>
+    <App />
+</div>, container);
